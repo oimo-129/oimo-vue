bc0eef82b489aaca2b61568d676fa8d924de67c2
朱德成 <81302913+oimo-129@users.noreply.github.com>
2025-03-23 11:40:37 +0800

    更新配置文件

--- vue后台管理项目/src/App.vue
+++ vue后台管理项目/src/App.vue
@@ -1,13 +1,23 @@
 <template>
-  <div>
+  <div class="box">
     <h1>后端管理Vue</h1>
   </div>
 </template>
 
 <script setup lang="ts">
 // 这里可以添加组件逻辑
+const str = '我爱你祖国'
 </script>
 
 <style>
-
+.box {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5f5f5;
+  color: #333;
+  font-family: Arial, sans-serif;
+}
 </style>
--- vue后台管理项目/src/main.ts
+++ vue后台管理项目/src/main.ts
@@ -4,4 +4,10 @@ import App from './App.vue'
 
 createApp(App).mount('#app')
 
-var a =100;
+// var a =100;
+// 测试箭头函数
+const fun = () => {
+  console.log('我爱你祖国')
+}
+
+fun()
